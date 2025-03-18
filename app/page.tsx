9bc832485ca98ec5bbaabc7dca5a7204d26157bd
Nuroldin U. Pimping <pimping.nu72@s.msumain.edu.ph>
2025-03-18 16:04:08 +0800

    feat: update layout and styles for improved UI, add copyright notice

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 	return (
 		<div className="flex h-screen max-h-screen">
 			<section className="remove-scrollbar container my-auto">
-				<div className="sub-container max-w-(496px)">
+				<div className="sub-container max-w-[496px]">
 					<Image
 						src="/assets/icons/logo-full.svg"
 						height={1000}
@@ -16,6 +16,11 @@ export default function Home() {
 					/>
 					<ToggleTheme />
 					<PatientForm />
+					<div className="text-14-regular mt-20 flex justify-between">
+						<p className="justify-items-center text-dark-600 xl:text-left">
+							&copy; 2025 Saniti Health. All rights reserved.
+						</p>
+					</div>
 				</div>
 			</section>
 		</div>
